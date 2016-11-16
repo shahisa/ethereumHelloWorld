a1c8fb8321bf22dabe70431a732a052fc00d11dd
Isaiah Weaver <iswea1@morgan.edu>
2016-11-16 03:51:09 -0500

    a simple contract

---
+++ contracts/helloWorld.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.4;
+contract helloWorld{
+    uint public balance;
+    
+    //contructor
+    //runs once upon contract creation
+    
+    function helloWorld(){
+        balance = 100;
+    }
+}
